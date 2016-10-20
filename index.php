e2c415dca5c2dc95cff8c43a550c085609a64e25
zhenjiang.dou <zhenjiang.dou@nextcont.com>
2016-10-20 11:26:54 +0800

    代码格式化
    今天改的都是小的测试使用的

--- index.php
+++ index.php
@@ -1,6 +1,5 @@
 <?php
-//基本参数
-define('SYS_ROOT',dirname(__file__));//本文件所在目录
-define('SYS_CORE',SYS_ROOT.'/core');//系统核心目录
-define('SYS_DATA',SYS_ROOT.'/data');//系统数据目录
-include SYS_CORE.'/initrd.php';//系统初始化
+define('SYS_ROOT',dirname(__file__)); // 本文件所在目录
+define('SYS_CORE',SYS_ROOT.'/core');  // 系统核心目录
+define('SYS_DATA',SYS_ROOT.'/data');  // 系统数据目录
+include SYS_CORE.'/initrd.php';       // 系统初始化
